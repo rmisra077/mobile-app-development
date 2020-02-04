811f003a3bfe5d45954ed6c66ed955727d31b79a
rmisra077 <rmisra077@gmail.com>
2020-02-03 22:17:05 -0500

    Added elements to Quiz app

--- QuizApp/app/src/main/java/com/misrarishabh/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/misrarishabh/quizapp/MainActivity.java
@@ -2,13 +2,38 @@ package com.misrarishabh.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button submitButton;
+    EditText responseText;
+    TextView displayText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton = findViewById(R.id.clickButton);
+        responseText = findViewById(R.id.responseEditText);
+        displayText = findViewById(R.id.textBox);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //System.out.println("Hello mom!");
+                Log.i("testButton", "Hi TJ! " + responseText.getText().toString());
+                displayText.setText(displayText.getText().toString()+" "+responseText.getText().toString());
+            }
+        });
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+
+            }
+        });
     }
 }
