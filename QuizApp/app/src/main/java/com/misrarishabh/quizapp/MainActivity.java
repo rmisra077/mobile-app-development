d55237924b46a0acb5eacd8e0bd102d08b6a299d
rmisra077 <rmisra077@gmail.com>
2020-02-06 09:53:05 -0500

    Completed Lab01 (Button app)

--- QuizApp/app/src/main/java/com/misrarishabh/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/misrarishabh/quizapp/MainActivity.java
@@ -1,19 +1,27 @@
 package com.misrarishabh.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.graphics.Color;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
     EditText responseText;
     TextView displayText;
+    Button colorButton;
+    ConstraintLayout currentLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,19 +29,40 @@ public class MainActivity extends AppCompatActivity {
         submitButton = findViewById(R.id.clickButton);
         responseText = findViewById(R.id.responseEditText);
         displayText = findViewById(R.id.textBox);
+        colorButton = findViewById(R.id.colorButton);
+        currentLayout = findViewById(R.id.background);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //System.out.println("Hello mom!");
-                Log.i("testButton", "Hi TJ! " + responseText.getText().toString());
-                displayText.setText(displayText.getText().toString()+" "+responseText.getText().toString());
+                int currnum = Integer.parseInt(displayText.getText().toString());
+                currnum += 1;
+                Log.i("testButton", responseText.getText().toString());
+                displayText.setText(currnum + "");
             }
         });
-        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        colorButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
 
+            public void onClick(View v) {
+                Random rand = new Random();
+                int r = rand.nextInt(256);
+                int g = rand.nextInt(256);
+                int b = rand.nextInt(256);
+                currentLayout.setBackgroundColor(Color.rgb(r, g, b));
             }
         });
+//        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                if(!hasFocus) {
+//                    if(responseText.getText().toString().equals("TJ")) {
+//                        displayText.setText("TJ Rocks!");
+//                        responseText.setText("");
+//                        responseText.setHint("That's a good name");
+//                    }
+//                }
+//            }
+//        });
     }
 }
