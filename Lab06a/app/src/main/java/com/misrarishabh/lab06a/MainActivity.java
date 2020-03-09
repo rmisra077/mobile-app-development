104c3be4b64cea59f44758484f191e29921ea5cb
rmisra077 <rmisra077@gmail.com>
2020-03-09 09:03:57 -0400

    Worked on custom app project part 1 (6a)

--- Lab06a/app/src/main/java/com/misrarishabh/lab06a/MainActivity.java
+++ Lab06a/app/src/main/java/com/misrarishabh/lab06a/MainActivity.java
@@ -5,10 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public static String translateWord(String word) {
+        String lowerCaseWord = word.toLowerCase();
+        int pos = -1; // Position of first vowel
+        char ch;
+
+        // This for loop finds the index of the first vowel in the word
+        for (int i = 0; i < lowerCaseWord.length(); i++) {
+            ch = lowerCaseWord.charAt(i);
+
+            if (isVowel(ch)) {
+                pos = i;
+                break;
+            }
+        }
+
+        if (pos == 0) {
+            // Translating word if the first character is a vowel (Rule 3)
+            return lowerCaseWord + "yay"; // Adding "yay" to the end of string (can also be "way" or just "ay")
+        } else {
+            // Translating word if the first character(s) are consonants (Rule 1 and 2)
+            String a = lowerCaseWord.substring(pos); // Extracting all characters in the word beginning from the 1st vowel
+            String b = lowerCaseWord.substring(0, pos); // Extracting all characters located before the first vowel
+            return a + b + "ay"; // Adding "ay" at the end of the extracted words after joining them.
+        }
+
+    }
+    public static Boolean isVowel(char ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'y') {
+            return true;
+        }
+        return false;
     }
 }
